555bcbbd2e332dca2e4f147ca73c0c8573dc6809
KorniDoS <76786248+KorniDoS@users.noreply.github.com>
2022-03-05 10:53:19 +0200

    Update resize.js

--- resize.js
+++ resize.js
@@ -3,7 +3,6 @@ const laptop = document.querySelector("#laptop");
 
 
 const onResize = () => {
-    //Fix menu bug where if the user clicked on the mobile menu button and switched to a higher resolution the links won't appear
     const mediaQuery = window.matchMedia("(min-width: 1000px)");
     const mediaMaxQuery = window.matchMedia("(max-width: 999px)");
     if (mediaQuery.matches) {
